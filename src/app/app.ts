40fe4509c3b569faee666578a1dc13d113745423
Satwik Bhavanari <shiva.satwik@sisainfosec.com>
2025-08-07 12:54:09 +0530

    Counter

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
+import { ProfileComponent } from './profile/profile';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [Login, Signup, ProfileComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('prod');
+  count = 0;
+
+  handleIncrement() {
+    this.count = this.count + 1;
+  }
+
+  handleDecrment() {
+    if (this.count > 0) {
+      this.count = this.count - 1;
+    }
+  }
+
+  handleReset() {
+    this.count = 0;
+  }
 }
